0f02b97527e073166a64cd1934f586df9e4b0687
Aravinds2511 <aravindvasang@gmail.com>
2026-02-24 08:31:00 +0000

    feat: completed max of array in yul

--- src/MaxOfArray.sol
+++ src/MaxOfArray.sol
@@ -7,6 +7,50 @@ contract MaxOfArray {
             // your code here
             // return the maximum value in the array
             // revert if array is empty
+            let len := mload(arr)
+            if eq(len,0) {
+                revert(0x00, 0x00)
+            }
+            let max := mload(add(arr, 0x20))
+            let val
+            let i := 1
+            for {} lt(i, len) {i := add(i, 1)} {
+                val := mload(add(arr, add(mul(i, 0x20), 0x20)))
+                if lt(max, val) {
+                    max := val
+                }
+            }
+            mstore(0x00, max)
+            return(0x00, 0x20)
         }
     }
 }
+
+//////////// GAS OPTIMIZED ///////////////
+
+// contract MaxOfArray {
+//     function main(uint256[] memory arr) external pure returns (uint256) {
+//         assembly {
+//             let len := mload(arr)
+//             if iszero(len) { revert(0, 0) }
+
+//             // p points to arr[0]
+//             let p := add(arr, 0x20)
+//             let end := add(p, mul(len, 0x20))
+
+//             // max = arr[0]
+//             let max := mload(p)
+
+//             // advance to arr[1]
+//             p := add(p, 0x20)
+
+//             for { } lt(p, end) { p := add(p, 0x20) } {
+//                 let v := mload(p)
+//                 if gt(v, max) { max := v }
+//             }
+
+//             mstore(0x00, max)
+//             return(0x00, 0x20)
+//         }
+//     }
+// }
